4520c7a6f6609d082badf9cb26c2adee4dcae5b0
Jon Fast <jnfstdj656@gmail.com>
2021-02-05 14:31:03 -0500

    create test methods.

--- LoanCompliance.Tests/UnitTest.cs
+++ LoanCompliance.Tests/UnitTest.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using LoanCompliance.BusinessLogic.Impl;
+using LoanCompliance.Data.InMemory.Impl;
+using LoanCompliance.Models.Api;
+using LoanCompliance.Models.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LoanCompliance.Tests
@@ -5,9 +10,36 @@ namespace LoanCompliance.Tests
     [TestClass]
     public class UnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        public void TestValidValidLoanComplianceInputs(decimal loanAmount, decimal apr, State state, LoanType loanType, LoanOccupancyType occupancyType, List<LoanFeeAllocation> feeAllocations)
         {
+            var processed = QuickBuild(loanAmount, apr, state, loanType, occupancyType, feeAllocations);
+            Assert.AreEqual(processed.Success, true);
+        }
+
+        [DataTestMethod]
+        public void TestInvalidLoanComplianceInputs(decimal loanAmount, decimal apr, State state, LoanType loanType, LoanOccupancyType occupancyType, List<LoanFeeAllocation> feeAllocations)
+        {
+            var processed = QuickBuild(loanAmount, apr, state, loanType, occupancyType, feeAllocations);
+            Assert.AreEqual(processed.Success, false);
+        }
+
+        private static ComplianceResult QuickBuild(decimal loanAmount, decimal apr, State state, LoanType loanType,
+            LoanOccupancyType occupancyType, List<LoanFeeAllocation> feeAllocations)
+        {
+            var query = new ComplianceQuery
+            {
+                LoanAmount = loanAmount,
+                AnnualPercentageRate = apr,
+                State = state,
+                LoanType = loanType,
+                OccupancyType = occupancyType,
+                FeeAllocations = feeAllocations
+            };
+            var dataAccess = new InMemoryDataAccess();
+            var apiFacade = new ApiFacade(dataAccess);
+            var processed = apiFacade.ProcessConformanceStep(query);
+            return processed;
         }
     }
 }
